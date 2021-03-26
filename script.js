038b606851095f752802b6c9d54ad5639d549b9c
Maksim Makarov <therealbrookebond@gmail.com>
2021-03-26 19:37:15 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
