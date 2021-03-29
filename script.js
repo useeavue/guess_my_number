be98dd90f95a8ec5ab17935d6c94785375f29154
Maksim Makarov <therealbrookebond@gmail.com>
2021-03-29 15:03:39 +0400

    drying out

--- script.js
+++ script.js
@@ -1,58 +1,67 @@
 'use strict';
 
+let guessNum = document.querySelector('.guess');
+let message = document.querySelector('.message');
+let scoreNum = document.querySelector('.score');
+let checkBtn = document.querySelector('.check');
+let bodyStyle = document.querySelector('body');
+let num = document.querySelector('.number');
+let highscoreNum = document.querySelector('.highscore');
+let again = document.querySelector('.again');
+
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let highscore = 0;
 let score = 20;
 
-document.querySelector('.check').addEventListener('click', () => {
-  let guess = Number(document.querySelector('.guess').value);
+checkBtn.addEventListener('click', () => {
+  let guess = Number(guessNum.value);
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    message.textContent = 'No number!';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number!';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.check').style.display = 'none';
+    message.textContent = 'Correct Number!';
+    bodyStyle.style.backgroundColor = '#60b347';
+    num.textContent = secretNumber;
+    checkBtn.style.display = 'none';
 
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      highscoreNum.textContent = highscore;
     }
 
-    document.querySelector('.number').style.width = '30rem';
+    num.style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
+      message.textContent = 'Too high!';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreNum.textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = 'red';
-      document.querySelector('.check').style.display = 'none';
+      message.textContent = 'You lost the game!';
+      scoreNum.textContent = 0;
+      bodyStyle.style.backgroundColor = 'red';
+      checkBtn.style.display = 'none';
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
+      message.textContent = 'Too low!';
       score--;
-      document.querySelector('.score').textContent = score;
+      scoreNum.textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'You lost the game!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = 'red';
-      document.querySelector('.check').style.display = 'none';
+      message.textContent = 'You lost the game!';
+      scoreNum.textContent = 0;
+      bodyStyle.style.backgroundColor = 'red';
+      checkBtn.style.display = 'none';
     }
   }
 });
 
-document.querySelector('.again').addEventListener('click', () => {
+again.addEventListener('click', () => {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.guess').value = '';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.message').textContent = 'Start guessig...';
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.check').style.display = 'block';
+  guessNum.value = '';
+  scoreNum.textContent = score;
+  bodyStyle.style.backgroundColor = '#222';
+  message.textContent = 'Start guessig...';
+  num.textContent = '?';
+  num.style.width = '15rem';
+  checkBtn.style.display = 'block';
 });
